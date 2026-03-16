73a6b7abb7dda96e8880a0f2496cc7e3fe450b83
FrancisGray-Chou <FrancisGray233@outlook.com>
2026-03-16 15:10:30 +0800

    Initial Commit

---
+++ YJS97/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  YJS97
+//
+//  Created by FrancisGray on 2026/3/16.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
